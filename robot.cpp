a28c3c055f9871568b22ac94bb7e4ab4df0db223
kaminno <honza.erny@gmail.com>
2022-11-20 17:25:39 +0100

    update class

--- robot.cpp
+++ robot.cpp
@@ -15,9 +15,19 @@ Robot::Robot(double x, double y, double z)
     this->currentPosition->setVectorCoordinates(x, y, z);
 }
 
+Robot::Robot(Vector s)
+: Robot() {
+    this->currentPosition->setVectorCoordinates(s);
+}
+
 Robot::Robot(double x, double y, double z, double v_x, double v_y, double v_z)
 : Robot(x, y, z) {
     this->currentLinearVelocity->setVectorCoordinates(v_x, v_y, v_z);
+}
+
+Robot::Robot(Vector s, Vector v)
+: Robot(s) {
+    this->currentLinearVelocity->setVectorCoordinates(v);
 }
 
 Robot::~Robot(){
@@ -35,5 +45,90 @@ unsigned int Robot::getNumberOfRobots(){
 }
 
 Vector Robot::getCurrentPosition(){
-    return Vector(this->currentPosition->getX(), 2, 2);
+    return this->currentPosition->getVectorCoordinates();
+    // double x = this->currentPosition->getX();
+    // double y = this->currentPosition->getY();
+    // double z = this->currentPosition->getZ();
+    // return Vector(x, y, z);
+}
+
+Vector Robot::getCurrentLinearVelocity(){
+    return this->currentLinearVelocity->getVectorCoordinates();
+    // double v_x = this->currentLinearVelocity->getX();
+    // double v_y = this->currentLinearVelocity->getY();
+    // double v_z = this->currentLinearVelocity->getZ();
+    // return Vector(v_x, v_y, v_z);
+}
+
+Vector Robot::getCurrentAcceleration(){
+    return this->currentAcceleration->getVectorCoordinates();
+    // double a_x = this->currentAcceleration->getX();
+    // double a_y = this->currentAcceleration->getY();
+    // double a_z = this->currentAcceleration->getZ();
+    // return Vector(a_x, a_y, a_z);
+}
+
+double Robot::getMaxVelocity(){
+    return this->maxVelocity;
+}
+
+double Robot::getMaxAcceleration(){
+    return this->maxAcceleration;
+}
+
+double Robot::getCurrentHeading(){
+    return this->currentHeading;
+}
+
+void Robot::setPosition(double x, double y, double z){
+    this->currentPosition->setVectorCoordinates(x, y, z);
+}
+
+void Robot::setPosition(Vector s){
+    this->currentPosition->setVectorCoordinates(s);
+}
+
+void Robot::setVelocity(double v_x, double v_y, double v_z){
+    double newVelocitySize = Vector::vectorNorm(v_x, v_y, v_z);
+    if(newVelocitySize <= this->maxVelocity){
+        this->currentLinearVelocity->setVectorCoordinates(v_x, v_y, v_z);
+    }
+}
+
+void Robot::setVelocity(Vector v){
+    double newVelocitySize = Vector::vectorNorm(v);
+    if(newVelocitySize <= this->maxVelocity){
+        this->currentLinearVelocity->setVectorCoordinates(v);
+    }
+}
+
+void Robot::setAcceleration(double a_x, double a_y, double a_z){
+    double newAccelerationSize = Vector::vectorNorm(a_x, a_y, a_z);
+    if(newAccelerationSize <= this->maxAcceleration){
+        this->currentAcceleration->setVectorCoordinates(a_x, a_y, a_z);
+    }
+}
+
+void Robot::setAcceleration(Vector a){
+    double newAccelerationSize = Vector::vectorNorm(a);
+    if(newAccelerationSize <= this->maxAcceleration){
+        this->currentAcceleration->setVectorCoordinates(a);
+    }
+}
+
+void Robot::setHeading(double heading){
+    // make sure that new heading is in correct range <0, 2pi)
+    this->currentHeading = heading;
+}
+
+void Robot::setMaxVelocity(double maxVelocity){
+    if(maxVelocity >= 0){
+        this->maxVelocity = maxVelocity;
+    }
+}
+
+void Robot::setMaxAcceleration(double maxAcceleration){
+    if(maxAcceleration >= 0){
+        this->maxAcceleration = maxAcceleration;
+    }
 }
--- robot.h
+++ robot.h
@@ -1,3 +1,6 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
 #include "vector_t.h"
 
 class Robot{
@@ -7,18 +10,35 @@ class Robot{
         Vector* currentPosition;
         Vector* currentLinearVelocity;
         Vector* currentAcceleration;
+        double currentHeading;
         double maxVelocity;
         double maxAcceleration;
-        unsigned int heading;
     public:
         Robot();
         Robot(double x, double y, double z);
+        Robot(Vector s);
         Robot(double x, double y, double z, double v_x, double v_y, double v_z);
+        Robot(Vector s, Vector v);
         ~Robot();
         unsigned int getId();
         unsigned int getNumberOfRobots();
         Vector getCurrentPosition();
-
-
-        // Position* position = new Position(3, 3, 4);
+        Vector getCurrentLinearVelocity();
+        Vector getCurrentAcceleration();
+        double getCurrentHeading();
+        double getMaxVelocity();
+        double getMaxAcceleration();
+        // maybe let the set methods return vector size to easy check if we achieve its maximum or not
+        void setPosition(double x, double y, double z);
+        void setPosition(Vector s);
+        void setVelocity(double v_x, double v_y, double v_z);
+        void setVelocity(Vector v);
+        void setAcceleration(double a_x, double a_y, double a_z);
+        void setAcceleration(Vector a);
+        // where the angle 0 rad is pointing and if we counted right handed system or not
+        void setHeading(double heading);
+        void setMaxVelocity(double maxVelocity);
+        void setMaxAcceleration(double maxAcceleration);
 };
+
+#endif
