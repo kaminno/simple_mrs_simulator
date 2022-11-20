2d424e162c0d10a34335f22521f3d61b474d5cea
kaminno <honza.erny@gmail.com>
2022-11-20 17:25:55 +0100

    update class vector_t

--- vector_t.cpp
+++ vector_t.cpp
@@ -1,6 +1,9 @@
 #include <cmath>
 #include "vector_t.h"
 
+Vector::Vector()
+: x(0), y(0), z(0) {}
+
 Vector::Vector(double x, double y, double z){
     this->x = x;
     this->y = y;
@@ -19,6 +22,10 @@ double Vector::getY(){
 
 double Vector::getZ(){
     return this->z;
+}
+
+Vector Vector::getVectorCoordinates(){
+    return Vector(this->x, this->y, this->z);
 }
 
 double Vector::getVectorSize(){
@@ -43,6 +50,16 @@ void Vector::setVectorCoordinates(double x, double y, double z){
     this->z = z;
 }
 
+void Vector::setVectorCoordinates(Vector v){
+    this->x = v.getX();
+    this->y = v.getY();
+    this->z = v.getZ();
+}
+
 double Vector::vectorNorm(double x, double y, double z){
     return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+}
+
+double Vector::vectorNorm(Vector v){
+    return sqrt(pow(v.getX(), 2) + pow(v.getY(), 2) + pow(v.getZ(), 2));
 }
--- vector_t.h
+++ vector_t.h
@@ -1,18 +1,27 @@
+#ifndef VECTOR_T_H
+#define VECTOR_T_H
+
 class Vector{
     private:
         double x;
         double y;
         double z;
     public:
-        Vector(double x = 0, double y = 0, double z = 0);
+        Vector();
+        Vector(double x, double y, double z);
         ~Vector();
         double getX();
         double getY();
         double getZ();
+        Vector getVectorCoordinates();
         double getVectorSize();
         void setX(double x);
         void setY(double y);
         void setZ(double z);
         void setVectorCoordinates(double x, double y, double z);
+        void setVectorCoordinates(Vector v);
         static double vectorNorm(double x, double y, double z);
+        static double vectorNorm(Vector v);
 };
+
+#endif
